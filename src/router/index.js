0c296b264a5f3bfc6f452e3d083c718f99ab7ed4
GuillaumeDoan <43437042+GuillaumeDoan@users.noreply.github.com>
2019-04-29 23:25:08 +0200

    Adding files.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
+import Home1 from '@/components/Home1'
 
 Vue.use(Router)
 
@@ -9,7 +9,7 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: Home1
     }
   ]
 })
